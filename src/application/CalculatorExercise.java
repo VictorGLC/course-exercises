5306eaf9a2e57a7527facdf08dbc53b4d9128d41
Victor Costa <vg47683@gmail.com>
2021-11-30 19:55:27 -0300

    adding java project to github repository

---
+++ src/application/CalculatorExercise.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.util.Scanner;
+
+public class CalculatorExercise {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String[] vect = new String[2];
+		String calculation = sc.nextLine();
+		
+		vect = calculation.split(" ");
+		
+		double numberOne = Double.parseDouble(vect[0]);
+		String symbol = vect[1];
+		double numberTwo = Double.parseDouble(vect[2]);
+		calculate(numberOne, symbol, numberTwo);
+		
+		sc.close();
+	}
+	
+	public static void calculate(double numberOne, String symbol, double numberTwo) {
+		System.out.print("O resultado da sua equação é: ");
+		if("+".equals(symbol)) {
+			System.out.print(numberOne + numberTwo);
+		} else if("-".equals(symbol)) {
+			System.out.print(numberOne - numberTwo);
+		}else if("*".equals(symbol)) {
+			System.out.print(numberOne * numberTwo);
+		} else if("/".equals(symbol)) {
+			System.out.print(numberOne / numberTwo);
+		}
+	}
+}
---
+++ src/entities/Employee.java
@@ -0,0 +1,48 @@
+package entities;
+
+public class Employee {
+	private Integer id;
+	private String name;
+	private double salary;
+	
+	public Employee() {
+	}
+	
+	public Employee(Integer id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public void increaseSalary(double percentage) {
+		salary += salary * (percentage/100);
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String toString() {
+		return id + ", " + name + ", " + salary;
+	}
+}

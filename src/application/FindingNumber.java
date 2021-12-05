3aaff4f0cff6a907cbaace32a1a2882ab69ff6e7
Victor Costa <vg47683@gmail.com>
2021-12-05 19:49:19 -0300

    adding class finding number

---
+++ src/application/FindingNumber.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Scanner;
+
+public class FindingNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Insert the array size: ");
+		int n = sc.nextInt();
+		int[] vect = new int[n];
+		
+		System.out.print("Add the array list: ");
+		for (int i=0; i<vect.length; i++) {
+				vect[i]= sc.nextInt();
+		}
+		System.out.print("Insert the number to be found: ");
+		int numberToFind= sc.nextInt();
+		int sum = 0;
+		
+		for (int i=0; i<vect.length; i++) {
+			if(vect[i] == numberToFind) {
+				sum++;
+			}
+		}
+		System.out.println("This vector has " + sum + " number " + numberToFind);
+		
+		sc.close();
+	}
+}
